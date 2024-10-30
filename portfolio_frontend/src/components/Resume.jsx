70f8e5b2afd6621462406fd9d546497da3026e5d
MyCloudAtl <megandmashburn@gmail.com>
2024-10-29 21:03:02 -0400

    Additions and modifications to resume

--- portfolio_frontend/src/components/Resume.jsx
+++ portfolio_frontend/src/components/Resume.jsx
@@ -30,8 +30,82 @@ const Resume = () => {
           </ul>
         </div>
       </section>  
+
+      <section className="resume-section">
+        <h2 className="ResumeH2">Projects:</h2>
+        <div className="Resume-item">
+<h4 className="ResumeH3"><u>Project Title: React Portfolio</u><span className="font-weight-normal">Sep 2024 - Oct 2024</span></h4>
+<h5 className="ResumeH4"><span className="location-font-weight">Technologies Used: </span></h5>
+<ul>
+    <li><b>Frontend:</b> Bootstrap, CSS3, HTML5, JavaScript(ES6+), React.js, Vite, React Router, React Hooks (useState, useEffect)</li>
+    <li><b>Backend:</b> Body-Parser, CORS, Express.js, Morgan, Node.js</li>
+    <li><b>Version Control:</b> GitHub</li>
+</ul>
+<h5 className="ResumeH4"><span className="location-font-weight">Description:</span></h5>
+<p> Generated a fully responsive personal portfolio website to showcase my projects, skills, and experience as a software engineer.  The application features modern design with intuitive navigation, enhancing user experience by improving usability.</p>
+<h5 className="ResumeH4"><span className="location-font-weight">Key Features:</span></h5>
+<ul>
+    <li>Enhanced user experience with animations using Framer Motion, adding engaging transitions and effects to components.</li>
+    <li>Incorpoarted a contact form for easy communication using EmailJS, utilizing backend APIs for form submission.</li>
+    <li>Utilized React Router for smooth navigation between different sections of the portfolio, including About, Projects, and Contact pages.</li>
+</ul>
+</div>
+<div className="Resume-item">
+<h4 className="ResumeH3"><u>Project Title: XploreATL</u> <span className="font-weight-normal">Jul 2024</span></h4>
+<h5 className="ResumeH4"><span className="location-font-weight">Technologies Used: </span></h5>
+<ul>
+    <li><b>Frontend:</b> Axios, CSS3, Font Awesome, HTML5, JavaScript(ES6+), Passport(Passport-Local & Passport-local-Mongoose), React.js, React Hooks (useState, useEffect)</li>
+    <li><b>Backend:</b> MongoDB, REST Framework for building APIs</li>
+    <li><b>Version Control:</b> GitHub</li>
+</ul>
+<h5 className="ResumeH4"><span className="location-font-weight">Wireframes & Documentation: </span></h5>
+<ul>
+    <li><a href="https://www.figma.com/board/qNRbo0XPas0OSJEJwKSiwY/Untitled?node-id=1-132&t=yufTu4Oy8VwMQYBf-0" target="_blank">Figma ERD/Component Diagram</a></li>
+    <li><a href="https://www.figma.com/board/oBLN9kVHmQsbu99fMCJkOk/Untitled?node-id=0-1&t=R80mUlvWTTm8p1wD-0" target="_blank">Figma Wireframe</a></li>
+    <li><a href="https://trello.com/b/pk6DEKuh/xploreatl" target="_blank">Trello Board</a></li>
+</ul>
+<h5 className="ResumeH4"><span className="location-font-weight">Description:</span></h5>
+<p>
+    XploreATL is a web application designed for locals and travelers to discover and explore various eateries and hotspots in their proximity. Users can log and save their data while exploring Metro-Atlanta, Georgia. Detailed information about each place, including descriptions, locations, operating hours, and contact details, will be available once the page loads. Users can mark places as favorites or bookmark them for future reference.
+</p>
+<h5 className="ResumeH4"><span className="location-font-weight">Key Features:</span></h5>
+<ul>
+    <li>Integrated secure user authentication and authorization, including registration, login/logout functionality, and user profiles.</li>
+    <li>Designed with scalability in mind, allowing for potential integration with maps, user reviews, and enhanced search features.</li>
+    <li>Implented fratures that allow users to mark their favorite places and bookmark locations for future reference, enhancing the exploration experience.</li>
+</ul>
+<h5 className="ResumeH4"><span className="location-font-weight">Link: <a href="https://github.com/MyCloudAtl/New-XploreATL.git"target="_blank">XploreATL</a></span></h5>
+</div>
+
+<div className="Resume-item">
+<h4 className="ResumeH3"><u>(Group) Project Title: Health-Fit</u><span className="font-weight-normal">Jun 2024</span></h4>
+        <h5 className="ResumeH4"><span className="location-font-weight">Technologies Used:</span></h5>
+          <ul>
+            <li><b>Frontend:</b> Axios, Bootstrap, CSS3(Flexbox/Grid), HTML5, React.js, React-Big-Calendar(Date-fns & DatePicker), React Router</li>
+            <li><b>Middleware:</b> Body-Parser, Morgan</li>
+            <li><b>Backend:</b> CORS, Express.js, MongoDB (Mongoose), Node.js</li>
+          </ul> 
+        <h5 className="ResumeH4"><span className="location-font-weight">Wireframes & Documentation:</span></h5>
+          <ul>
+            <li><a href="https://lucid.app/lucidchart/685c5c43-f02e-400e-ad44-514a08ea487d/edit?viewport_loc=-33%2C-479%2C2219%2C1061%2C0_0&invitationId=inv_b93ec5cd-b833-4b7d-bb7b-fbe60882eec9" target="_blank">Lucid ERD/Component Diagram</a></li>
+            <li><a href="https://lucid.app/lucidchart/a378afd9-b738-490a-b79a-ada9dcfa27f5/edit?viewport_loc=-480%2C-465%2C3898%2C1865%2C0_0&invitationId=inv_a89989df-38a3-4d55-b367-0897b8300be5" target="_blank">Lucid Wireframe</a></li>
+          </ul>
+        <h5 className="ResumeH4"><span className="location-font-weight">Description:</span></h5>
+        <p>
+            Developed a full-stack web application enabling users to record, store, and manage their personal health intake and fitness goals. The application features a clean, responsive design, allowing users to log daily intake, track gym activities, and manage their fitness journey through a user-friendly interface.
+        </p>
+        <h5 className="ResumeH4"><span className="location-font-weight">Key Features:</span></h5>
+        <ul>
+            <li>Created full CRUD functionalities across front and backend for users to create, view, update, and delete health logs.</li>
+            <li>Developed a RESTful API for seamless front-end and back-end communication, utilizing Axios for data fetching.</li>
+            <li>Designed and implemented a responsive UI using React and Bootstrap for optimal user experience across devices.</li>
+            <li>Utilized GitHub for version control and Trello for team collaboration and project management.</li>
+        </ul>
+        <h5 className="ResumeH4"><span className="location-font-weight">Link: <a href="https://github.com/MyCloudAtl/Health-Fit.git"target="_blank">Health-Fit</a></span></h5>
+        </div>
+      </section>  
       
-      <section className="Resume-section">
+      <section className="resume-section">
         <h2 className="ResumeH2">Professional Experience:</h2>
         <div className="Resume-item">
           <h3 className="ResumeH3">Senior Full Desk Account Executive <span className="font-weight-normal">Oct 2022 - Apr 2024</span></h3>
